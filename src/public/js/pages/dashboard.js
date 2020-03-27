b6cdae4089fad10bda56b691c9976b45ecc8f572
coregameHD <coregamehd@coregameHDs-Macbook-Pro-16.local>
2020-03-27 20:23:13 +0700

    fix 'undefined' value in pop-up + code refactor

--- src/public/js/pages/dashboard.js
+++ src/public/js/pages/dashboard.js
@@ -1,261 +1,3 @@
-$(function () {
-  'use strict'
-
-  // Make the dashboard widgets sortable Using jquery UI
-  $('.connectedSortable').sortable({
-    placeholder         : 'sort-highlight',
-    connectWith         : '.connectedSortable',
-    handle              : '.card-header, .nav-tabs',
-    forcePlaceholderSize: true,
-    zIndex              : 999999
-  })
-  $('.connectedSortable .card-header, .connectedSortable .nav-tabs-custom').css('cursor', 'move')
-
-  // jQuery UI sortable for the todo list
-  $('.todo-list').sortable({
-    placeholder         : 'sort-highlight',
-    handle              : '.handle',
-    forcePlaceholderSize: true,
-    zIndex              : 999999
-  })
-
-  // bootstrap WYSIHTML5 - text editor
-  $('.textarea').summernote()
-
-  $('.daterange').daterangepicker({
-    ranges   : {
-      'Today'       : [moment(), moment()],
-      'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-      'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-      'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-      'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-    },
-    startDate: moment().subtract(29, 'days'),
-    endDate  : moment()
-  }, function (start, end) {
-    window.alert('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-  })
-
-  /* jQueryKnob */
-  $('.knob').knob()
-
-  // jvectormap data
-  var visitorsData = {
-    'US': 398, //USA
-    'SA': 400, //Saudi Arabia
-    'CA': 1000, //Canada
-    'DE': 500, //Germany
-    'FR': 760, //France
-    'CN': 300, //China
-    'AU': 700, //Australia
-    'BR': 600, //Brazil
-    'IN': 800, //India
-    'GB': 320, //Great Britain
-    'RU': 3000 //Russia
-  }
-  // World map by jvectormap
-  $('#world-map').vectorMap({
-    map              : 'usa_en',
-    backgroundColor  : 'transparent',
-    regionStyle      : {
-      initial: {
-        fill            : 'rgba(255, 255, 255, 0.7)',
-        'fill-opacity'  : 1,
-        stroke          : 'rgba(0,0,0,.2)',
-        'stroke-width'  : 1,
-        'stroke-opacity': 1
-      }
-    },
-    series           : {
-      regions: [{
-        values           : visitorsData,
-        scale            : ['#ffffff', '#0154ad'],
-        normalizeFunction: 'polynomial'
-      }]
-    },
-    onRegionLabelShow: function (e, el, code) {
-      if (typeof visitorsData[code] != 'undefined')
-        el.html(el.html() + ': ' + visitorsData[code] + ' new visitors')
-    }
-  })
-
-  // Sparkline charts
-  var sparkline1 = new Sparkline($("#sparkline-1")[0], {width: 80, height: 50, lineColor: '#92c1dc', endColor: '#ebf4f9'});
-  var sparkline2 = new Sparkline($("#sparkline-2")[0], {width: 80, height: 50, lineColor: '#92c1dc', endColor: '#ebf4f9'});
-  var sparkline3 = new Sparkline($("#sparkline-3")[0], {width: 80, height: 50, lineColor: '#92c1dc', endColor: '#ebf4f9'});
-
-  sparkline1.draw([1000, 1200, 920, 927, 931, 1027, 819, 930, 1021]);
-  sparkline2.draw([515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921]);
-  sparkline3.draw([15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21]);
-
-  // The Calender
-  $('#calendar').datetimepicker({
-    format: 'L',
-    inline: true
-  })
-
-  // SLIMSCROLL FOR CHAT WIDGET
-  $('#chat-box').overlayScrollbars({
-    height: '250px'
-  })
-
-  /* Chart.js Charts */
-  // Sales chart
-  var salesChartCanvas = document.getElementById('revenue-chart-canvas').getContext('2d');
-  //$('#revenue-chart').get(0).getContext('2d');
-
-  var salesChartData = {
-    labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label               : 'Digital Goods',
-        backgroundColor     : 'rgba(60,141,188,0.9)',
-        borderColor         : 'rgba(60,141,188,0.8)',
-        pointRadius          : false,
-        pointColor          : '#3b8bba',
-        pointStrokeColor    : 'rgba(60,141,188,1)',
-        pointHighlightFill  : '#fff',
-        pointHighlightStroke: 'rgba(60,141,188,1)',
-        data                : [28, 48, 40, 19, 86, 27, 90]
-      },
-      {
-        label               : 'Electronics',
-        backgroundColor     : 'rgba(210, 214, 222, 1)',
-        borderColor         : 'rgba(210, 214, 222, 1)',
-        pointRadius         : false,
-        pointColor          : 'rgba(210, 214, 222, 1)',
-        pointStrokeColor    : '#c1c7d1',
-        pointHighlightFill  : '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
-        data                : [65, 59, 80, 81, 56, 55, 40]
-      },
-    ]
-  }
-
-  var salesChartOptions = {
-    maintainAspectRatio : false,
-    responsive : true,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        gridLines : {
-          display : false,
-        }
-      }],
-      yAxes: [{
-        gridLines : {
-          display : false,
-        }
-      }]
-    }
-  }
-
-  // This will get the first returned node in the jQuery collection.
-  var salesChart = new Chart(salesChartCanvas, {
-      type: 'line',
-      data: salesChartData,
-      options: salesChartOptions
-    }
-  )
-
-  // Donut Chart
-  var pieChartCanvas = $('#sales-chart-canvas').get(0).getContext('2d')
-  var pieData        = {
-    labels: [
-        'Instore Sales',
-        'Download Sales',
-        'Mail-Order Sales',
-    ],
-    datasets: [
-      {
-        data: [30,12,20],
-        backgroundColor : ['#f56954', '#00a65a', '#f39c12'],
-      }
-    ]
-  }
-  var pieOptions = {
-    legend: {
-      display: false
-    },
-    maintainAspectRatio : false,
-    responsive : true,
-  }
-  //Create pie or douhnut chart
-  // You can switch between pie and douhnut using the method below.
-  var pieChart = new Chart(pieChartCanvas, {
-    type: 'doughnut',
-    data: pieData,
-    options: pieOptions
-  });
-
-  // Sales graph chart
-  var salesGraphChartCanvas = $('#line-chart').get(0).getContext('2d');
-  //$('#revenue-chart').get(0).getContext('2d');
-
-  var salesGraphChartData = {
-    labels  : ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2', '2012 Q3', '2012 Q4', '2013 Q1', '2013 Q2'],
-    datasets: [
-      {
-        label               : 'Digital Goods',
-        fill                : false,
-        borderWidth         : 2,
-        lineTension         : 0,
-        spanGaps : true,
-        borderColor         : '#efefef',
-        pointRadius         : 3,
-        pointHoverRadius    : 7,
-        pointColor          : '#efefef',
-        pointBackgroundColor: '#efefef',
-        data                : [2666, 2778, 4912, 3767, 6810, 5670, 4820, 15073, 10687, 8432]
-      }
-    ]
-  }
-
-  var salesGraphChartOptions = {
-    maintainAspectRatio : false,
-    responsive : true,
-    legend: {
-      display: false,
-    },
-    scales: {
-      xAxes: [{
-        ticks : {
-          fontColor: '#efefef',
-        },
-        gridLines : {
-          display : false,
-          color: '#efefef',
-          drawBorder: false,
-        }
-      }],
-      yAxes: [{
-        ticks : {
-          stepSize: 5000,
-          fontColor: '#efefef',
-        },
-        gridLines : {
-          display : true,
-          color: '#efefef',
-          drawBorder: false,
-        }
-      }]
-    }
-  }
-
-  // This will get the first returned node in the jQuery collection.
-  var salesGraphChart = new Chart(salesGraphChartCanvas, {
-      type: 'line',
-      data: salesGraphChartData,
-      options: salesGraphChartOptions
-    }
-  )
-
-});
-
-// Migrate parts of UVIS
 var api_url = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
 var mapbox_attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
 var my_access_token = 'pk.eyJ1Ijoid2FyYWtvcm5qZXQiLCJhIjoiY2syajc5aGdyMTJkMzNrcGNhZmNvazl4byJ9.VPwVxiXRD22W1xMhzwSw7g'
@@ -290,63 +32,62 @@ var weatherData = []
 var selectedNode = 0
 var array_marker = []
 
-var jangwad = [] //จังหวัด
-var JangwadLayerGroup = []
-var KLASSmarker;
+// A helper function to create a marker
+function createMarkerHelper(icon){
+  console.log("เริ่มดึงข้อมูลจาก server");
+  var dataFromServer = JSON.parse(document.getElementById("fuckingDataFromServer").innerText);
+
+  // Create the markerClusterGroup here
+  var markerGroups = L.markerClusterGroup();
+  var tempMarkerForLayer = [];
+  for (i in dataFromServer){
+    console.log("Node ลำกับที่ " + i);
+    console.log(dataFromServer[i]);
+
+    var title = dataFromServer[i].node_name
+    var marker =  L.marker(new L.LatLng(dataFromServer[i].location.coordinates[0], dataFromServer[i].location.coordinates[1]),{
+      title:title, icon: icon});
+    
+    // HTML สำหรับใส่ใน Pop-up
+    var html = "<bigtext>" + dataFromServer[i].node_name + "</bigtext>";
+    html += "<br>[" + dataFromServer[i].location.coordinates[0] + ", " + dataFromServer[i].location.coordinates[1] + "]<br>";
+
+    if (dataFromServer[i].data.hasOwnProperty('temperature')){
+      html += "<br><img src='images/thermometer.png' width='50px' height='50px'> <b>Temperature:</b> " + dataFromServer[i].data.temperature;
+    }
+    if (dataFromServer[i].data.hasOwnProperty('humidity')){
+      html += "<br><img src='images/humidity.png' width='50px' height='50px'> <b>Relative Humidity:</b> " + dataFromServer[i].data.humidity;
+    }
+    if (dataFromServer[i].data.hasOwnProperty('pm1')){
+      html += "<br><img src='images/pm1icon.png' width='50px' height='50px'> <b>PM1:</b> " + dataFromServer[i].data.pm1;
+    }
+    if (dataFromServer[i].data.hasOwnProperty('pm10')){
+      html += "<br><img src='images/pm10icon.png' width='50px' height='50px'> <b>PM10:</b> " + dataFromServer[i].data.pm10;
+    }
+    if (dataFromServer[i].data.hasOwnProperty('pm2_5')){
+      html += "<br><img src='images/pm2_5icon.png' width='50px' height='50px'> <b>PM2.5:</b> " + dataFromServer[i].data.pm2_5;
+    }
+
+    marker.bindPopup(html, {maxWidth: "400"});
+    markerGroups.addLayer(marker);
+    tempMarkerForLayer.push(marker);
+  }
+  return markerGroups;
+}
 
 //สร้าง marker ทดสอบที่ส่งมาจาก express ของลายเส้น
 function createFuckingClusterMarkers(){
   console.log("เริ่มสร้าง marker");
   var KLASSIcon = L.icon({
     iconUrl: 'images/KLASS.png',
-
-    iconSize:     [38, 50], // size of the icon
+    iconSize:     [38, 50], 
     iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
     //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-
-  console.log("ควย");
-  var fuckingTest = document.getElementById("fuckingDataFromServer").innerText;
-  console.log(fuckingTest);
-  var fuckingObj = JSON.parse(fuckingTest);
-
-  // Create the markerClusterGroup here
-  var markers3 = L.markerClusterGroup();
-  var tempMarkerForLayer = [];
-  //var tempLength = fuckingObj.length;
-  for(p in fuckingObj){
-    console.log("ทดสอบ2");
-    var node_point = fuckingObj[p];
-    var title = node_point.node_name
-    var marker =  L.marker(new L.LatLng(node_point.location.coordinates[0], node_point.location.coordinates[1]),{
-      title:title, icon: KLASSIcon
-    });
-    marker.bindPopup("<bigtext>" + node_point.node_name + "</bigtext>" +
-        "<br><img src='images/thermometer.png' width='50px' height='50px'> <b>Temperature:</b> " + node_point.data.temperature +
-        "<br><img src='images/humidity.png' width='50px' height='50px'> <b>Relative Humidity:</b> " + node_point.data.humidity +
-        "<br><img src='images/pm1icon.png' width='50px' height='50px'> <b>PM1:</b> " + node_point.data.pm1 +
-        "<br><img src='images/pm10icon.png' width='50px' height='50px'> <b>PM10:</b> " + node_point.data.pm10, {
-          maxWidth: "400"
-        });
-    markers3.addLayer(marker);
-    tempMarkerForLayer.push(marker);
-  }
-
-  layerControl.addOverlay(markers3, "DataFromServer");
-  mymap.addLayer(markers3);
+  var getAllMarkersFromServer = createMarkerHelper(KLASSIcon);
+  layerControl.addOverlay(getAllMarkersFromServer, "DataFromServer");
+  mymap.addLayer(getAllMarkersFromServer);
 }
 
-createFuckingClusterMarkers(); //เพิ่มเข้ามาใหม่ fucking
+createFuckingClusterMarkers(); //เพิ่มเข้ามาใหม่
 
-function onMapClick(e) {
-	alert("You clicked the map at " + e.latlng);
-}
-
-mymap.on('click', onMapClick);
-function onMapClick(e) {
-	popup
-		.setLatLng(e.latlng)
-		.setContent("You clicked the map at " + e.latlng.toString())
-		.openOn(mymap);
-}
-mymap.on('click', onMapClick);
