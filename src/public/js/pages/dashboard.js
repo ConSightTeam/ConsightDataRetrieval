40bb01e2e14b6aeb1fca08c6f5506b909178e5a5
wara_1234 <wadh1n1@hotmail.com>
2020-04-10 02:15:54 +0700

    Add function createHeatMaps() to dashboard.js

--- src/public/js/pages/dashboard.js
+++ src/public/js/pages/dashboard.js
@@ -92,4 +92,39 @@ function createFuckingClusterMarkers(){
   mymap.addLayer(getAllMarkersFromServer);
 }
 
+function creatHeatMaps()
+{
+  console.log("Begin creating the heatmap")
+  console.log("Retrieving data from back-end")
+  var testData = {
+    max: 8,
+    data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}, ...]
+  };
+  console.log("Finish data retrieval from back-end")
+
+  var heatmap_config = {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    // if scaleRadius is false it will be the constant radius used in pixels
+    "radius": 2,
+    "maxOpacity": .8,
+    // scales the radius based on map zoom
+    "scaleRadius": true,
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": true,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  };
+  
+  var heatmapLayer = new HeatmapOverlay(heatmap_config)
+  heatmapLayer.setData(testData)
+  layerControl.addOverlay(heatmapLayer, "Heatmap")
+}
+
+createHeatMaps()
 createFuckingClusterMarkers(); //เพิ่มเข้ามาใหม่
