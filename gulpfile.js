efd2fb54d9c50685d6df248a39450986544e2b4a
coregameHD <coregamehd@coregameHDs-Macbook-Pro-16.local>
2020-04-09 20:21:55 +0700

    Install 'gulp-uglifycss'

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const install = require('gulp-install');
 const tsProject = ts.createProject('tsconfig.json');
+var uglifycss = require('gulp-uglifycss');
 
 const PROD_DEST = 'dist';
 
@@ -29,8 +30,18 @@ function public() {
         .pipe(gulp.dest(PROD_DEST + '/public'));
 }
 
+function copyCSS() {
+    return gulp.src('src/css/**/*.css')
+          .pipe(uglifycss({
+            "maxLineLen": 80,
+            "uglyComments": true
+          }))
+          .pipe(gulp.dest(PROD_DEST + '/public/css'));
+}
+
 exports.transpile = transpile;
 exports.copyDependency = copyDependency;
 exports.copyHandlebars = copyHandlebars;
 exports.public = public;
-exports.default = gulp.parallel(transpile, copyHandlebars, copyDependency, public);
+exports.copyCSS = copyCSS;
+exports.default = gulp.parallel(transpile, copyHandlebars, copyDependency, public, copyCSS);
