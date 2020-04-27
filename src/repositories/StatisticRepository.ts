20cad93198609667159a41ded6b686c91af6db90
Laizen Paptiti <59090008@kmitl.ac.th>
2020-04-27 22:45:35 +0700

    Add query mode and historical data

--- src/repositories/StatisticRepository.ts
+++ src/repositories/StatisticRepository.ts
@@ -7,25 +7,135 @@ export class StatisticRepository {
         this.reset();
     }
 
-    public async getStatisticFromSpecificDate(property: string, unit: string, date: Date): Promise<Statistic> {
+    public async getStatisticsPerDay(property: string, unit: string): Promise<Array<Statistic>> {
         await this.db.connect();
-        let queryResult = await this.db.query(
+        let queryResult: QueryResult<Statistic> = await this.db.query(
             "SELECT \
-                MAX(data.property) as maximum, MIN(data.property) as minimum, AVG(data.property) as average, data.unit as unit, inserted_on::date as inserted_on_date \
+                MAX(data.property) as maximum, MIN(data.property) as minimum, AVG(data.property) as average, data.unit as unit, data.inserted_on::date as inserted_on_date \
             FROM  \
                 ( \
                     SELECT \
-                        cast(split_part(data ->> $1, ' ', 1) as integer) as property, split_part(data ->> $1, ' ', 2) as unit, inserted_on \
+                        cast(split_part(data ->> $1, ' ', 1) as numeric) as property, split_part(data ->> $1, ' ', 2) as unit, inserted_on \
                     FROM \
                         data_point \
-                    WHERE data ->> $1 IS NOT NULL AND inserted_on::date = $3 \
+                    WHERE data ->> $1 IS NOT NULL \
                 ) AS data \
             WHERE data.unit = $2 \
-            GROUP BY data.unit, data.inserted_on::date;", [property, unit, date]);
+            GROUP BY data.unit, data.inserted_on::date \
+            ORDER BY data.inserted_on::date DESC \
+            LIMIT 100;", [property, unit]);
         await this.db.end();
 
-        return queryResult.rows[0] as Statistic;
-    };
+        return queryResult.rows;
+    }
+
+    public async getStatisticsPerHour(property: string, unit: string): Promise<Array<Statistic>> {
+        await this.db.connect();
+        let queryResult: QueryResult<Statistic> = await this.db.query(
+            "SELECT \
+                MAX(data.property) as maximum, MIN(data.property) as minimum, AVG(data.property) as average, data.unit as unit, date_trunc('hour', data.inserted_on) as inserted_on_date \
+            FROM  \
+                ( \
+                    SELECT \
+                        cast(split_part(data ->> $1, ' ', 1) as numeric) as property, split_part(data ->> $1, ' ', 2) as unit, inserted_on \
+                    FROM \
+                        data_point \
+                    WHERE data ->> $1 IS NOT NULL \
+                ) AS data \
+            WHERE data.unit = $2 \
+            GROUP BY data.unit, date_trunc('hour', data.inserted_on) \
+            ORDER BY date_trunc('hour', data.inserted_on) DESC \
+            LIMIT 100;", [property, unit]);
+        await this.db.end();
+
+        return queryResult.rows;
+    }
+
+    public async getStatisticsPerMonth(property: string, unit: string): Promise<Array<Statistic>> {
+        await this.db.connect();
+        let queryResult: QueryResult<Statistic> = await this.db.query(
+            "SELECT \
+                MAX(data.property) as maximum, MIN(data.property) as minimum, AVG(data.property) as average, data.unit as unit, date_trunc('month', data.inserted_on) as inserted_on_date \
+            FROM  \
+                ( \
+                    SELECT \
+                        cast(split_part(data ->> $1, ' ', 1) as numeric) as property, split_part(data ->> $1, ' ', 2) as unit, inserted_on \
+                    FROM \
+                        data_point \
+                    WHERE data ->> $1 IS NOT NULL \
+                ) AS data \
+            WHERE data.unit = $2 \
+            GROUP BY data.unit, date_trunc('month', data.inserted_on) \
+            ORDER BY date_trunc('month', data.inserted_on) DESC \
+            LIMIT 100;", [property, unit]);
+        await this.db.end();
+
+        return queryResult.rows;
+    }
+
+    public async getStatisticsPerDayOnCertainMonth(property: string, unit: string, date: Date): Promise<Array<Statistic>> {
+        await this.db.connect();
+        let queryResult: QueryResult<Statistic> = await this.db.query(
+            "SELECT \
+                MAX(data.property) as maximum, MIN(data.property) as minimum, AVG(data.property) as average, data.unit as unit, data.inserted_on::date as inserted_on_date \
+            FROM  \
+                ( \
+                    SELECT \
+                        cast(split_part(data ->> $1, ' ', 1) as numeric) as property, split_part(data ->> $1, ' ', 2) as unit, inserted_on \
+                    FROM \
+                        data_point \
+                    WHERE data ->> $1 IS NOT NULL \
+                ) AS data \
+            WHERE data.unit = $2 AND date_trunc('month', data.inserted_on::date) = date_trunc('month', $3::date) \
+            GROUP BY data.unit, data.inserted_on::date \
+            ORDER BY date_trunc('month', data.inserted_on::date) DESC;", [property, unit, date]);
+        await this.db.end();
+
+        return queryResult.rows;
+    }
+
+    public async getStatisticsPerHourOnCertainDay(property: string, unit: string, date: Date): Promise<Array<Statistic>> {
+        await this.db.connect();
+        let queryResult: QueryResult<Statistic> = await this.db.query(
+            "SELECT \
+                MAX(data.property) as maximum, MIN(data.property) as minimum, AVG(data.property) as average, data.unit as unit, date_trunc('hour', data.inserted_on) as inserted_on_date \
+            FROM  \
+                ( \
+                    SELECT \
+                        cast(split_part(data ->> $1, ' ', 1) as numeric) as property, split_part(data ->> $1, ' ', 2) as unit, inserted_on \
+                    FROM \
+                        data_point \
+                    WHERE data ->> $1 IS NOT NULL \
+                ) AS data \
+            WHERE data.unit = $2 AND data.inserted_on::date = $3::date \
+            GROUP BY data.unit, date_trunc('hour', data.inserted_on) \
+            ORDER BY date_trunc('hour', data.inserted_on) DESC;", [property, unit, date]);
+        await this.db.end();
+
+        return queryResult.rows;
+    }
+
+    public async getStatisticsPerMonthOnCertainYear(property: string, unit: string, date: Date): Promise<Array<Statistic>> {
+        await this.db.connect();
+        let queryResult: QueryResult<Statistic> = await this.db.query(
+            "SELECT \
+                MAX(data.property) as maximum, MIN(data.property) as minimum, AVG(data.property) as average, date_trunc('month', data.inserted_on) as inserted_on_date \
+            FROM  \
+                ( \
+                    SELECT \
+                        cast(split_part(data ->> $1, ' ', 1) as numeric) as property, split_part(data ->> $1, ' ', 2) as unit, inserted_on \
+                    FROM \
+                        data_point \
+                    WHERE data ->> $1 IS NOT NULL \
+                ) AS data \
+            WHERE data.unit = $2 AND date_trunc('year', data.inserted_on::date) = date_trunc('year', $3::date) \
+            GROUP BY data.unit, date_trunc('month', data.inserted_on) \
+            ORDER BY date_trunc('month', data.inserted_on) DESC;", [property, unit, date]);
+        await this.db.end();
+
+        return queryResult.rows;
+    }
+
 
     public reset() {
         this.db = new Client();
--- src/routes/statistic.ts
+++ src/routes/statistic.ts
@@ -1,30 +1,47 @@
 import { Router } from "express";
 import { StatisticRepository } from "../repositories/StatisticRepository";
+import { domainToASCII } from "url";
 var router = Router();
 
-router.get('/pm2_5', async function(req, res, next) {
+async function queryLatestBasedOnCertainTime(mode: string, property: string, unit: string, date_str?: string): Promise<Statistic[]> {
   let dao = new StatisticRepository(); 
-  res.render('statistic', { display_property: "PM2.5", statistic: await dao.getStatisticFromSpecificDate('pm2_5', 'μg/m^3', new Date()) });
+  if (date_str) {
+    let date: Date = new Date(date_str);
+    console.log(date);
+    switch (mode) {
+      case 'hour': return await dao.getStatisticsPerHourOnCertainDay(property, unit, date);
+      case 'day': return await dao.getStatisticsPerDayOnCertainMonth(property, unit, date);
+      case 'month': return await dao.getStatisticsPerMonthOnCertainYear(property, unit, date);
+      default: return null;;
+    } 
+  } else {
+    switch (mode) {
+      case 'hour': return await dao.getStatisticsPerHour(property, unit);
+      case 'day': return await dao.getStatisticsPerDay(property, unit);
+      case 'month': return await dao.getStatisticsPerMonth(property, unit);
+      default: return null;;
+    } 
+  }
+}
+
+router.get('/pm2_5/:mode', async function(req, res, next) {
+  res.render('statistic', { display_property: "PM2.5", statistics: await queryLatestBasedOnCertainTime(req.params['mode'], 'pm2_5', 'μg/m^3', req.query['date']) });
 });
 
-router.get('/pm10', async function(req, res, next) {
-  let dao = new StatisticRepository(); 
-  res.render('statistic', { display_property: "PM10", statistic: await dao.getStatisticFromSpecificDate('pm10', 'μg/m^3', new Date()) });
+router.get('/pm10/:mode', async function(req, res, next) {
+  res.render('statistic', { display_property: "PM10", statistics: await queryLatestBasedOnCertainTime(req.params['mode'], 'pm10', 'μg/m^3', req.query['date']) });
 });
 
-router.get('/pm1', async function(req, res, next) {
-  let dao = new StatisticRepository(); 
-  res.render('statistic', { display_property: "PM1", statistic: await dao.getStatisticFromSpecificDate('pm1', 'μg/m^3', new Date()) });
+router.get('/pm1/:mode', async function(req, res, next) {
+  res.render('statistic', { display_property: "PM1", statistics: await queryLatestBasedOnCertainTime(req.params['mode'], 'pm1', 'μg/m^3', req.query['date']) });
 });
 
-router.get('/temperature', async function(req, res, next) {
-  let dao = new StatisticRepository(); 
-  res.render('statistic', { display_property: "Temperature", statistic: await dao.getStatisticFromSpecificDate('temperature', 'c', new Date()) });
+router.get('/temperature/:mode', async function(req, res, next) {
+  res.render('statistic', { display_property: "Temperature", statistics: await queryLatestBasedOnCertainTime(req.params['mode'], 'temperature', 'c', req.query['date']) });
 });
 
-router.get('/humidity', async function(req, res, next) {
-  let dao = new StatisticRepository(); 
-  res.render('statistic', { display_property: "Humidity", statistic: await dao.getStatisticFromSpecificDate('humidity', '%', new Date()) });
+router.get('/humidity/:mode', async function(req, res, next) {
+  res.render('statistic', { display_property: "Humidity", statistics: await queryLatestBasedOnCertainTime(req.params['mode'], 'humidity', '%', req.query['date']) });
 });
 
 module.exports = router;
