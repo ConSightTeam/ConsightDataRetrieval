91536990f997a0769cba1e8e87f8cac1fb5625a0
coregameHD <coregamehd@coregameHDs-Macbook-Pro-16.local>
2020-05-16 00:20:05 +0700

    UI  Improvements

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -9,6 +9,11 @@ function createMarkerHelper(icon){
 
   // Create the markerClusterGroup here
   var markerGroups = L.markerClusterGroup();
+  var customPopupOptions = {
+    'maxWidth': '500',
+    'className': 'custom-popup'
+  }
+
   var tempMarkerForLayer = [];
   for (i in dataFromServer){
     var title = dataFromServer[i].node_name
@@ -17,30 +22,29 @@ function createMarkerHelper(icon){
     
     // Insert pop-up in HTML
     var html = "<bigtext>" + dataFromServer[i].node_name + "</bigtext>";
-    html += "<br>[" + dataFromServer[i].location.coordinates[0] + ", " + dataFromServer[i].location.coordinates[1] + "]<br>";
+    html += "<br>[" + dataFromServer[i].location.coordinates[0] + ", " + dataFromServer[i].location.coordinates[1] + "]<br>" ;
+    html += "<br><b>Node Owner:</b> " + dataFromServer[i].owner + "<br>";
 
     if (dataFromServer[i].data.hasOwnProperty('temperature')){
-      html += "<br><img src='images/statusicon/thermometer.png' width='50px' height='50px'> <b>Temperature:</b> " + dataFromServer[i].data.temperature;
+      html += "<br><img src='images/statusicon/thermometer.png' width='50px' height='50px'> <b>&nbspTemperature:</b> " + dataFromServer[i].data.temperature;
     }
     if (dataFromServer[i].data.hasOwnProperty('humidity')){
-      html += "<br><img src='images/statusicon/humidity.png' width='50px' height='50px'> <b>Relative Humidity:</b> " + dataFromServer[i].data.humidity;
+      html += "<br><img src='images/statusicon/humidity.png' width='50px' height='50px'> <b>&nbspRelative Humidity:</b> " + dataFromServer[i].data.humidity;
     }
     if (dataFromServer[i].data.hasOwnProperty('co_density')){
-      html += "<br><img src='images/statusicon/sand.png' width='50px' height='50px'> <b>Density:</b> " + dataFromServer[i].data.co_density;
+      html += "<br><img src='images/statusicon/sand.png' width='50px' height='50px'> <b>&nbspDensity:</b> " + dataFromServer[i].data.co_density;
     }
     if (dataFromServer[i].data.hasOwnProperty('pm1')){
-      html += "<br><img src='images/statusicon/pm1icon.png' width='50px' height='50px'> <b>PM1:</b> " + dataFromServer[i].data.pm1;
+      html += "<br><img src='images/statusicon/pm1icon.png' width='50px' height='50px'> <b>&nbspPM1:</b> " + dataFromServer[i].data.pm1;
     }
     if (dataFromServer[i].data.hasOwnProperty('pm10')){
-      html += "<br><img src='images/statusicon/pm10icon.png' width='50px' height='50px'> <b>PM10:</b> " + dataFromServer[i].data.pm10;
+      html += "<br><img src='images/statusicon/pm10icon.png' width='50px' height='50px'> <b>&nbspPM10:</b> " + dataFromServer[i].data.pm10;
     }
     if (dataFromServer[i].data.hasOwnProperty('pm2_5')){
-      html += "<br><img src='images/statusicon/pm2_5icon.png' width='50px' height='50px'> <b>PM2.5:</b> " + dataFromServer[i].data.pm2_5;
+      html += "<br><img src='images/statusicon/pm2_5icon.png' width='50px' height='50px'> <b>&nbspPM2.5:</b> " + dataFromServer[i].data.pm2_5;
     }
 
-    html += "<br>Node Owner: " + dataFromServer[i].owner + "<br>";
-
-    marker.bindPopup(html, {maxWidth: "400"});
+    marker.bindPopup(html, customPopupOptions);
     markerGroups.addLayer(marker);
     tempMarkerForLayer.push(marker);
   }
@@ -51,8 +55,8 @@ function createClusterMarkers(){
   var NodeIcon = L.icon({
     iconUrl: 'images/Node_dark.png',
     iconSize:     [38, 50], 
-    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
-    //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconAnchor:   [0, 0],
+    popupAnchor:  [22, -4]
 });
   var getAllMarkersFromServer = createMarkerHelper(NodeIcon);
   layerControl.addOverlay(getAllMarkersFromServer, "DataFromServer");
