bdd14598de49d80edc8deda0634d86a42f48bfbb
Laizen Paptiti <59090008@kmitl.ac.th>
2020-05-16 17:53:10 +0700

    Add ability to export statistics as CSV file

--- src/routes/statistic.ts
+++ src/routes/statistic.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatisticRepository } from "../repositories/StatisticRepository";
+import { ExportToCsv } from 'export-to-csv';
 var router = Router();
 
 const display_properties = {
@@ -9,42 +10,62 @@ const display_properties = {
   temperature: "Temperature",
   humidity: "Humidity",
   co_density: "Co Density"
+};
+
+function getStatistics(date_str: string, property: string, unit: string, mode: string): Promise<Array<Statistic>> {
+  let dao = new StatisticRepository();
+
+  if (date_str) {
+    let date: Date = new Date(date_str);
+    switch (mode) {
+      case 'hour': return dao.getStatisticsPerHourOnCertainDay(property, unit, date);
+      case 'day': return dao.getStatisticsPerDayOnCertainMonth(property, unit, date);
+      case 'month': return dao.getStatisticsPerMonthOnCertainYear(property, unit, date);
+      default: throw new Error('Invalid Mode');
+    }
+  } else {
+    switch (mode) {
+      case 'hour': return dao.getStatisticsPerHour(property, unit);
+      case 'day': return dao.getStatisticsPerDay(property, unit);
+      case 'month': return dao.getStatisticsPerMonth(property, unit);
+      default: throw new Error('Invalid Mode');
+    }
+  }
 }
 
-router.get('/:property/:unit/:mode', async function(req: Request, res: Response, next: NextFunction) {
+router.get('/:property/:unit/:mode', async function (req: Request, res: Response, next: NextFunction) {
   let date_str: string = req.query['date'];
   let property: string = req.params['property'];
   let unit: string = req.params['unit'];
   let mode: string = req.params['mode'];
 
-  let statistics: Array<Statistic>;
-  let dao = new StatisticRepository();
-
   try {
-    if (date_str) {
-      let date: Date = new Date(date_str);
-      switch (mode) {
-        case 'hour': statistics = await dao.getStatisticsPerHourOnCertainDay(property, unit, date); break;
-        case 'day': statistics = await dao.getStatisticsPerDayOnCertainMonth(property, unit, date); break;
-        case 'month': statistics = await dao.getStatisticsPerMonthOnCertainYear(property, unit, date); break;
-        default: throw new Error('Invalid Mode');
-      }
-    } else {
-      switch (mode) {
-        case 'hour': statistics = await dao.getStatisticsPerHour(property, unit); break;
-        case 'day': statistics = await dao.getStatisticsPerDay(property, unit); break;
-        case 'month': statistics = await dao.getStatisticsPerMonth(property, unit); break;
-        default: throw new Error('Invalid Mode');
-      }
-    }
+    res.render('statistic', { statistics: await getStatistics(date_str, property, unit, mode), display_property: display_properties[property] || property, mode: mode });
   } catch (e) {
     console.error(e.stack);
     return next(e);
   }
-
-  res.render('statistic', { statistics: statistics, display_property: display_properties[property] || property });
 });
 
+router.get('/:property/:unit/:mode/export', async function (req: Request, res: Response, next: NextFunction) {
+  let exporter = new ExportToCsv({ useKeysAsHeaders: true });
 
+  let date_str: string = req.query['date'];
+  let property: string = req.params['property'];
+  let unit: string = req.params['unit'];
+  let mode: string = req.params['mode'];
+
+  res.setHeader('Content-Type', 'application/octet-stream');
+  res.setHeader("content-disposition", "attachment; filename=\"export.csv\"");
+
+  try {
+    let statistics = await getStatistics(date_str, property, unit, mode);
+    res.send(exporter.generateCsv(statistics, true));
+    res.end();
+  } catch (e) {
+    console.error(e.stack);
+    return next(e);
+  }
+});
 
 module.exports = router;
